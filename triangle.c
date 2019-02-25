bbdec5faf0e05ac3c4ce9ff932be7b356f23f596
CMOI <parisnicolas.ifs@gmail.com>
2019-02-25 20:51:37 +0100

    TRI1

---
+++ triangle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  int base;
+  int hauteur;
+  printf("TRIANGLE \n");
+  printf("base: ");
+  scanf("%d\n", base);
+  printf("hauteur: ");
+  scanf("%d\n", hauteur);
+printf("%d %d\n", base, hauteur);
+  return 0;
+}
