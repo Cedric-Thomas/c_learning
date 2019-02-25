c061c563fd2800ab1c8cbb9f68900eb43c80b80c
Cédric <cedric.thomas@protonmail.com>
2019-02-25 21:29:36 +0100

    correction

--- ctriangle.c
+++ ctriangle.c
@@ -1,19 +1,43 @@
 #include <stdio.h>
 
 int main(void) {
+
   int x, y;
   int base, hauteur;
+
   printf("BASE => ");
   scanf("%d", &base);
   fflush(stdin);
+  if(base%2 == 0)
+  {
+    printf("BASE CANNOT BE PAIR !\n");
+    base++;
+  }
   printf("HAUTEUR => ");
   scanf("%d", &hauteur);
   fflush(stdin);
   printf("TRIANGLE: B=%d, H=%d\n", base, hauteur);
 
   for (y=0; y < hauteur; y++) {
-    for(x=0; x < base; x++) {
-      printf("X");
+    for (x=0; x < base; x++) {
+      if (y==0 && x==base/2) {
+        printf("|");
+      }
+      else if (x==(base/2)-y) {
+        printf("/");
+      }
+      else if (x==(base/2)+y) {
+        printf("\\");
+      }
+      else if (y==base/2      && \
+               x > (base/2)-y && \
+               x < (base/2)+y)
+      {
+        printf("-");
+      }
+      else {
+        printf(" ");
+      }
     }
     printf("\n");
   }
