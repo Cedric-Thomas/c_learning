0d76bd5baa5ef926d9327c0a98bc1dfa050126a4
Cédric <cedric.thomas@protonmail.com>
2019-02-25 21:08:54 +0100

    005

--- ctriangle.c
+++ ctriangle.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
 int main(void) {
-  int i, j;
+  int x, y;
   int base, hauteur;
   printf("BASE => ");
   scanf("%d", &base);
+  fflush(stdin);
   printf("HAUTEUR => ");
   scanf("%d", &hauteur);
+  fflush(stdin);
   printf("TRIANGLE: B=%d, H=%d\n", base, hauteur);
-  for (i=0; i < hauteur; i++) {
-    for(j=0; j < base; j++) {
+
+  for (y=0; y < hauteur; y++) {
+    for(x=0; x < base; x++) {
       printf("X");
     }
     printf("\n");
