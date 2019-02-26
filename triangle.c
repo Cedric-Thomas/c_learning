2c7c59b3146e065b34dd9fdb50757a051b66a66a
CMOI <parisnicolas.ifs@gmail.com>
2019-02-26 08:06:41 +0100

    cmieu

--- triangle.c
+++ triangle.c
@@ -10,17 +10,17 @@ int main() {
   printf("hauteur: ");
   scanf("%d", &hauteur);
 
-  if(base%2 ==0)
+  if(base%2 == 0)
     base++;
 
   printf("TRIANGLE: base=%d hauteur=%d\n", base, hauteur);
   for ( y = 0; y < hauteur; y++) {
     for (x = 1; x < base;  x++) {
-    if (y == 0 && x==base%2)
+    if (y == 0 && x==base/2)
     printf(".");
-    else if(x ==base%2-y)
+    else if(x ==base/2-y)
       printf("/");
-    else if(x == base%2+y)
+    else if(x ==base/2+y)
       printf("\\");
     else
       printf(" ");
