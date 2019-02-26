51de8e017e00a1caa48bd86739b8ebe173bba631
CMOI <parisnicolas.ifs@gmail.com>
2019-02-26 07:02:37 +0100

    gtest

--- triangle.c
+++ triangle.c
@@ -10,13 +10,21 @@ int main() {
   printf("hauteur: ");
   scanf("%d", &hauteur);
 
+  if(base%2 ==0)
+    base++;
+
   printf("TRIANGLE: base=%d hauteur=%d\n", base, hauteur);
-  for ( x = 0; x < hauteur; x++) {
-    printf("/");
-    for (y = 1; y < base;  y++) {
-        printf("-");
+  for ( y = 0; y < hauteur; y++) {
+    for (x = 1; x < base;  x++) {
+    if (y == 0 && x==base%2)
+    printf(".");
+    else if(x ==base%2-y)
+      printf("/");
+    else if(x == base%2+y)
+      printf("\\");
+    else
+      printf(" ");
     }
-  printf("\\");
   printf("\n");
 
   }
